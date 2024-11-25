cc9af93d782d4ae91db0c528eb4b067bbd4da224
big-b-man <diysandvich@gmail.com>
2024-11-25 12:15:55 -0800

    Messing with screen clearing

--- src/main.c
+++ src/main.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+void delaySeconds(double time){
+	clock_t start_time = clock();
+	while(clock() < (start_time + (int)(time*1000000)));
+}
 
 int main(){
-	printf("Hello World!\n");
+	for(int i = 0; i <= 100; i++){
+		printf("%i",i);
+		delaySeconds(.1);
+		printf("\x1B[2k\r");
+	}
 	return 0;
 }
