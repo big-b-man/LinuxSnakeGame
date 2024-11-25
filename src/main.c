c4adad1d043e5f57f08d5dd2d85c6d664926af28
big-b-man <diysandvich@gmail.com>
2024-11-25 15:07:45 -0800

    Updated main.c to test timing functions

--- src/main.c
+++ src/main.c
@@ -2,16 +2,23 @@
 #include <stdlib.h>
 #include <time.h>
 
-void delaySeconds(double time){
-	clock_t start_time = clock();
-	while(clock() < (start_time + (int)(time*1000000)));
+// Function for delaying in seconds
+void delaySeconds(double number_of_seconds) {
+    // Convert seconds to clock ticks
+    clock_t start_time = clock();
+    clock_t end_time = start_time + (clock_t)(number_of_seconds * CLOCKS_PER_SEC);
+
+    // Wait until the required time has passed
+    while (clock() < end_time)
+        ;
 }
 
 int main(){
 	for(int i = 0; i <= 100; i++){
 		printf("%i",i);
+		fflush(stdout);
 		delaySeconds(.1);
-		printf("\x1B[2k\r");
+		printf("\r\x1B[2K");
 	}
 	return 0;
 }
